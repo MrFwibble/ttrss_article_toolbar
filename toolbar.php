6282c669c3571413f67134e5366f9b3492d53b18
MrFwibble <dave@fwibble.org>
2018-12-12 20:33:42 +0000

    Update toolbar.php
    refractoring since changes to ttrss onclick events

--- toolbar.php
+++ toolbar.php
@@ -6,21 +6,15 @@ print_user_stylesheet($link);
 ?>
 <span id="framarticle-toolbar">
 
-    <button class="button_nav" title="Subscribe To Feed" onclick="quickAddFeed()">
-    <i class="icon-plus"></i></button>
-
-    <button class="button_nav" title="Toggle Expanded View" onclick="toggle_cdm_expanded()">
-    <i class="icon-th-list"></i></button>
-
     <button class="button_nav" title="Mark feed as read" onclick="catchupCurrentFeed()">
     <i class="icon-eye-open"></i></button>
 
-    <button class="button_nav" title="Refresh Feed" onclick="viewCurrentFeed()">
+    <button class="button_nav" title="Refresh Feed" onclick="Feeds.reloadCurrent()">
     <i class="icon-refresh"></i></button>
 
-    <button class="button_nav" title="Previous Post" onclick="moveToPost('prev',true)">
+    <button class="button_nav" title="Previous Post" onclick="Headlines.move('prev')">
     <i class="icon-arrow-up"></i></button>
 
-    <button class="button_nav" title="Next Post" onclick="moveToPost('next',true)">
+    <button class="button_nav" title="Next Post" onclick="Headlines.move('next')">
     <i class="icon-arrow-down"></i></button>
 </span>
